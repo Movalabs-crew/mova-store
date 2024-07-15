aeeabac9aefe4694cbdd48f9d670532e341f146e
OpensrcLord <287525836+OpensrcLord@users.noreply.github.com>
2024-07-15 14:07:53 +0100

    some changes

--- .next/static/webpack/webpack.07825a2c56851415.hot-update.js
+++ .next/static/webpack/webpack.07825a2c56851415.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "287df403f599d4e9"; }
+/******/ 	__webpack_require__.h = function() { return "538dc0c51f54dddf"; }
 /******/ }();
 /******/ 
 /******/ }
--- .next/static/webpack/webpack.446d4d2866416e17.hot-update.js
+++ .next/static/webpack/webpack.446d4d2866416e17.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d805dbf8c5679e57"; }
+/******/ 	__webpack_require__.h = function() { return "07825a2c56851415"; }
 /******/ }();
 /******/ 
 /******/ }
